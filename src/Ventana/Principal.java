d0d89afe950baf2908886271db69ff563ac7086b
Admin <Admin@DESKTOP-I0VL6TR>
2023-02-05 14:01:21 -0600

    Cree una ventana que suma dos numeros

---
+++ src/Ventana/Principal.java
@@ -0,0 +1,14 @@
+
+package Ventana;
+
+
+public class Principal 
+{
+    public static void main(String[] args)
+    {
+       //Crear el objeto ventana
+       Ventana miVentana = new Ventana();
+       //Mostrar la Ventana
+       miVentana.setVisible(true);
+    }
+}
---
+++ src/Ventana/Ventana.java
@@ -0,0 +1,93 @@
+package Ventana;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ventana extends JFrame
+{
+
+    public Ventana ()
+    {
+        //Crear ventana
+
+        //this.setSize(500, 500);
+        //Centrar ventana
+        //this.setLocationRelativeTo(null);
+        //Cerrar y dejar de ejecutar
+        this.setDefaultCloseOperation (EXIT_ON_CLOSE);
+
+        //Agregar titulo a la ventana
+        this.setTitle ("Mi primera vez con Java y Netbeans");
+
+        //this.setVisible(true);
+        //mover ventana a un lugar especifico
+        //this.setBounds(100, 200, 500, 500);
+        //Bloquear ventana en tamano
+        //this.setResizable(false);
+        //Maximizar ventana
+        this.setExtendedState (MAXIMIZED_BOTH);
+
+        //Pintar ventana
+        //this.getContentPane().setBackground(Color.blue);
+        //Llamar al metodo para inicializar los componentes
+        iniciarComponentes ();
+    }
+
+    private void iniciarComponentes ()
+    {
+        //Crear un panel
+        JPanel panel = new JPanel ();
+        panel.setBackground (Color.white);
+        this.getContentPane ().add (panel);
+
+        //Etiquedas
+        JLabel nombre = new JLabel ("Java y Netbeans");
+        nombre.setFont (new Font ("tipoLetra", Font.BOLD, 18));
+        nombre.setBounds (200, 100, 300, 40);
+        panel.add (nombre);
+
+                        //CODIGO EXTRA
+        // Crear dos JTextFields para ingresar los números
+        JTextField num1 = new JTextField ();
+        num1.setBounds (100, 100, 150, 20);
+        num1.setColumns (10);
+        panel.add (num1);
+
+        JTextField num2 = new JTextField ();
+        num2.setBounds (250, 100, 150, 20);
+        num2.setColumns (10);
+        panel.add (num2);
+
+// Crear un JButton para realizar la suma
+        JButton btnSumar = new JButton ("Sumar");
+        btnSumar.setBounds (150, 150, 100, 20);
+        panel.add (btnSumar);
+
+// Crear un JLabel para mostrar el resultado de la suma
+        JLabel resultado = new JLabel ();
+        resultado.setBounds (300, 100, 50, 20);
+        panel.add (resultado);
+
+// Añadir un evento al botón de suma
+        btnSumar.addActionListener (new ActionListener ()
+        {
+            @Override
+            public void actionPerformed (ActionEvent e)
+            {
+                int n1 = Integer.parseInt (num1.getText ());
+                int n2 = Integer.parseInt (num2.getText ());
+                int suma = n1 + n2;
+                resultado.setText (String.valueOf (suma));
+            }
+        });
+
+    }
+
+}
